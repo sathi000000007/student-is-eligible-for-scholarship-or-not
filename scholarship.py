8e764a0cd742bf2d23fa923ce9d565438a053a05
sathi000000007 <sathi7019756776@gmail.com>
2025-07-10 16:23:06 +0530

    Create scholarship.py

---
+++ scholarship.py
@@ -0,0 +1,9 @@
+marks=int(input("enter a marks"))
+if marks>=90:
+  area=input("area name")
+  if area=="rural area":
+   print("eligible for scholarship")
+  else:
+    print("not eligible")
+else:
+  print("not eligible for scholarship")
